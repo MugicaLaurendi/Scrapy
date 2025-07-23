e6d8116913a86dfc2a6ac432225a635f73482df0
chaimae <chaimaemehdaoui@gmail.com>
2025-07-23 16:53:25 +0200

    projet_chaimae

--- bricodepot_scraper/settings.py
+++ bricodepot_scraper/settings.py
@@ -8,14 +8,10 @@ ROBOTSTXT_OBEY = True
 
 FEED_EXPORT_ENCODING = 'utf-8'
 FEEDS = {
-    'categories.json': {
-        'format': 'json',
-        'overwrite': True,
-    }
+    'categories.csv': {
+        'format': 'csv',
+        'encoding': 'utf8',
+        'fields': ['category', 'subcategory', 'url', 'slug'],
+    },
 }
-BOT_NAME = "bricodepot_scraper"
 
-SPIDER_MODULES = ["bricodepot_scraper.spiders"]
-NEWSPIDER_MODULE = "bricodepot_scraper.spiders"
-
-ROBOTSTXT_OBEY = False
--- nettoyage.py
+++ nettoyage.py
@@ -1,24 +1,28 @@
-import json
+import csv
 
 def remove_duplicate_products_by_url(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        products = json.load(f)
-
     seen_urls = set()
     unique_products = []
 
-    for product in products:
-        url = product.get('url')
-        if url and url not in seen_urls:
-            seen_urls.add(url)
-            unique_products.append(product)
+    with open(input_file, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            url = row.get('url')
+            if url and url not in seen_urls:
+                seen_urls.add(url)
+                unique_products.append(row)
 
-    with open(output_file, 'w', encoding='utf-8') as f:
-        json.dump(unique_products, f, ensure_ascii=False, indent=4)
+    # Écrire le CSV nettoyé
+    if unique_products:
+        fieldnames = unique_products[0].keys()  # Garde les mêmes colonnes
+        with open(output_file, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(unique_products)
 
     print(f"Nettoyage terminé : {len(unique_products)} produits uniques enregistrés dans {output_file}")
 
 
 # Exemple d'utilisation
 if __name__ == '__main__':
-    remove_duplicate_products_by_url('products.json', 'produits_uniques.json')
+    remove_duplicate_products_by_url('products.csv', 'products_cleaned.csv')
